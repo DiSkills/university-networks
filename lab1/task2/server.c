f58d10566b11efe46e3da2a917d9dfffcba375c4
Arkady Sokolenko <github@diskills-developer.org>
2025-02-12 20:24:20 +0300

    lab1/task2/server.c: add fsm

--- lab1/task2/server.c
+++ lab1/task2/server.c
@@ -1,7 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "str.h"
+
+#if !defined(NUMBER_MARKS) || NUMBER_MARKS < 1
+#define NUMBER_MARKS 4
+#endif
+
+enum fsm_states {
+    fsm_start,
+    fsm_name = fsm_start,
+    fsm_mark,
+    fsm_finish,
+    fsm_error,
+};
+
+struct request_fsm {
+    enum fsm_states state;
+
+    char *name;
+
+    int marks[NUMBER_MARKS];
+    int marks_usage;
+};
+
+static void request_fsm_init(struct request_fsm *fsm)
+{
+    fsm->state = fsm_start;
+    fsm->name = NULL;
+    fsm->marks_usage = 0;
+}
+
+static void request_fsm_clear(struct request_fsm *fsm)
+{
+    fsm->state = fsm_start;
+    if (fsm->name) {
+        free(fsm->name);
+    }
+    fsm->name = NULL;
+    fsm->marks_usage = 0;
+}
+
+static void request_fsm_handle_mark(struct request_fsm *fsm, char *line)
+{
+    long mark;
+    char *endptr;
+
+    mark = strtol(line, &endptr, 10);
+    if (!*line || *endptr || mark < 2 || mark > 5) {
+        fsm->state = fsm_error;
+        return;
+    }
+
+    fsm->marks[fsm->marks_usage] = mark;
+    fsm->marks_usage++;
+    if (fsm->marks_usage >= NUMBER_MARKS) {
+        fsm->state = fsm_finish;
+        return;
+    }
+}
+
+static void request_fsm_step(struct request_fsm *fsm, char *line)
+{
+    switch (fsm->state) {
+    case fsm_name:
+        fsm->name = line;
+        fsm->state = fsm_mark;
+        return;
+    case fsm_mark:
+        request_fsm_handle_mark(fsm, line);
+        break;
+    case fsm_finish:
+        break;
+    case fsm_error:
+        break;
+    }
+    free(line);
+}
+
+static void server_handle_request(char *request)
+{
+    char *nl;
+    struct request_fsm fsm;
+    request_fsm_init(&fsm);
+
+    while ((nl = strchr(request, '\n')) != NULL) {
+        char *line;
+
+        *nl = '\0';
+        line = strdup(request);
+        request = nl + 1;
+
+        request_fsm_step(&fsm, line);
+    }
+    if (fsm.state == fsm_finish) {
+        /* TODO */
+    }
+
+    request_fsm_clear(&fsm);
+}
 
 static int server_run(char *requests, char *responses)
 {
@@ -17,8 +115,9 @@ static int server_run(char *requests, char *responses)
     for (;;) {
         int c = fgetc(req);
         if (c == EOF) {
-            /* TODO: handle the request */
+            server_handle_request(str->data);
 
+            str_clear(str);
             fclose(req);
             req = fopen(requests, "r");
         } else {
