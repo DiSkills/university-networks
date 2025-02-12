390b836311b233dd1f23d85af7e09fc15a24ee1c
Arkady Sokolenko <github@diskills-developer.org>
2025-02-12 19:33:02 +0300

    lab1/task2/client.c: add send_request

--- lab1/task2/client.c
+++ lab1/task2/client.c
@@ -1,16 +1,51 @@
+#include <fcntl.h>
 #include <stdio.h>
 
 #if !defined(NUMBER_MARKS) || NUMBER_MARKS < 1
 #define NUMBER_MARKS 4
 #endif
 
+static int send_request(const char *requests, const char *name, char **marks)
+{
+    int i;
+    FILE *f;
+
+    f = fopen(requests, "w");
+    if (!f) {
+        perror(requests);
+        return 0;
+    }
+
+    fprintf(f, "%s\n", name);
+    for (i = 0; i < NUMBER_MARKS; i++) {
+        fprintf(f, "%s\n", marks[i]);
+    }
+    fflush(f);
+
+    fclose(f);
+    return 1;
+}
+
 int main(int argc, char **argv)
 {
+    int ok;
+    char *requests, *responses, *name, **marks;
+
     if (argc != 4 + NUMBER_MARKS) {
         fprintf(stderr, "Expected: %s <requests> <responses> "
                 "<full-name> <marks>[%d]\n", argv[0], NUMBER_MARKS);
         return 1;
     }
+
+    requests = argv[1];
+    responses = argv[2];
+    name = argv[3];
+    marks = argv + 4;
+
+    ok = send_request(requests, name, marks);
+    if (!ok) {
+        return 2;
+    }
+
     return 0;
 }
-
